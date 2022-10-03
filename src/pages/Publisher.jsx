500397d203cbc7cdbf1b7a403a6e07ff05739f4f
theblackman2 <paskasonga@gmail.com>
2022-10-03 16:34:26 +0100

    Can see infos about a publisher

--- src/pages/Publisher.jsx
+++ src/pages/Publisher.jsx
@@ -1,10 +1,54 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import Loader from "../components/Loader";
+import Pagination from "../components/Pagination";
 import App from "../Layouts/App";
+const axios = require("axios").default;
 
 function Publisher() {
   const { id } = useParams();
-  const page = <Container>Hello from publisher {id}</Container>;
+  const [publisher, setPublisher] = useState({});
+  const [loadingPublisher, setLoadingPublisher] = useState(true);
+  const [books, setBooks] = useState([]);
+  const [loadingBooks, setLoadingBooks] = useState(true);
+
+  useEffect(() => {
+    const api = process.env.REACT_APP_API;
+    const publisher = axios.get(`${api}/publishers/${id}`);
+    publisher
+      .then((response) => setPublisher(response.data))
+      .then(() => setLoadingPublisher(false))
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  useEffect(() => {
+    const api = process.env.REACT_APP_API;
+    const books = axios.get(`${api}/publishers/${id}/books`);
+    books
+      .then((response) => setBooks(response.data))
+      .then(() => setLoadingBooks(false))
+      .catch((err) => console.log(err));
+  }, [id]);
+  const page =
+    loadingBooks || loadingPublisher ? (
+      <Loader />
+    ) : (
+      <Container>
+        {publisher.length > 0 ? (
+          <>
+            <div className="head">
+              <h2>{publisher[0].name}</h2>
+            </div>
+            <div className="body">
+              <Pagination items={books} itemsPerPage={5} type="books" />
+            </div>
+          </>
+        ) : (
+          <div>No publisher for this id</div>
+        )}
+      </Container>
+    );
   return <App page={page} />;
 }
 
